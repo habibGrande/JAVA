98af42a8fd7628293e245afc16f69d1159858ad2
habibGrande <snomo22dy@gmail.com>
2023-12-26 11:55:39 +0200

    first commit

---
+++ Java/Java_Fundamentals/First_Java/first_asg.java
@@ -0,0 +1,9 @@
+public class first_asg {
+    public static void main(String[] args) {
+        System.out.println("my name is Mohammad habib ");
+        System.out.println(" I am 22 years old ");
+        System.out.println("my hometown is Ramallah, palestine");
+ 
+    }
+    
+}
