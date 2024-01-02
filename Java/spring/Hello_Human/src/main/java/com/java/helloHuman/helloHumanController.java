7acc5d60a2bf3109f0f361141179151a567c8a89
habibGrande <snomo22dy@gmail.com>
2024-01-02 14:56:13 +0200

    hello human

--- Java/spring/Hello_Human/src/main/java/com/java/helloHuman/helloHumanController.java
+++ Java/spring/Hello_Human/src/main/java/com/java/helloHuman/helloHumanController.java
@@ -1,5 +1,28 @@
 package com.java.helloHuman;
 
-public class helloHumanController {
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class helloHumanController {
+	
+	
+	 @RequestMapping("/")
+	    public String hellohuman(){
+	    	return "Hello Human" ;
+	    }
+	 
+	 @RequestMapping("/name/{first_name}")
+	 public String name(@PathVariable("first_name") String first_name){
+		    	return "hello: " + first_name ;
+		    }
+		 
+	 @RequestMapping("/name/{first_name}/{last_name}")
+	 public String showLesson(@PathVariable("first_name") String first_name, @PathVariable("last_name") String last_name){
+		    	return "Hello " + first_name + " " + last_name;
+		    }
+		 
+		 
+	 
 }
