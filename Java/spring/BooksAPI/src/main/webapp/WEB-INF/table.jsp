d955b588dec37b0e4683a8e51f17852ac457622f
habibGrande <snomo22dy@gmail.com>
2024-01-11 11:38:44 +0200

    dojo ninjas

--- Java/spring/BooksAPI/src/main/webapp/WEB-INF/table.jsp
+++ Java/spring/BooksAPI/src/main/webapp/WEB-INF/table.jsp
@@ -27,7 +27,8 @@
 	         <c:forEach var="book" items="${books}">
 	         	<tr>
 	         		<td scope="row"><c:out value="${book.id}"/></td>
-	         		<td><c:out value="${book.title}"/></td>
+	         		<td><a ><c:out value="${book.title}"/>
+	         					</a></td>
 	         		<td><c:out value="${book.language}"/></td>
 	         		<td><c:out value="${book.numberOfPages}"/></td>
 	         	</tr>
--- Java/spring/DojoAndNinjas/src/main/java/com/java/DojoAndNinjas/Controllers/controller.java
+++ Java/spring/DojoAndNinjas/src/main/java/com/java/DojoAndNinjas/Controllers/controller.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.java.DojoAndNinjas.Services.DojoService;
@@ -59,9 +60,11 @@ public class controller {
     }
     
     @GetMapping("/dojos/{id}")
-    public String dojosS(@ModelAttribute("dojos") Dojo dojo,Model model ){
-		model.addAttribute("dojo", dojo);
-    	return "showDojo.jsp";
+    public String dojosS(@ModelAttribute("dojos") Dojo dojo,Model model ,@PathVariable("id") Long id ){
+    	Dojo dojos = dojoService.findDojo(id);
+    	
+		model.addAttribute("dojo", dojos);
+    	return "show.jsp";
     }
     
     
