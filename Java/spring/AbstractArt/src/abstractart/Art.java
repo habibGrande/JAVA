2a311bad54519d0930629c68d8fe4d436efea968
habibGrande <snomo22dy@gmail.com>
2024-01-16 17:03:27 +0200

    AbstractArt asg

---
+++ Java/spring/AbstractArt/src/abstractart/Art.java
@@ -0,0 +1,9 @@
+package abstractart;
+
+public abstract class Art {
+	String title;
+	String author;
+	String description;
+	
+	abstract void viewArt();
+}
---
+++ Java/spring/AbstractArt/src/abstractart/Painting.java
@@ -0,0 +1,10 @@
+package abstractart;
+
+public class Painting extends Art {
+	String paintType;
+	
+	public  void viewArt() {
+		System.out.println("This is a Painting.");
+	}
+
+}
